42f24fbb0093287d1abe43de31c71bad0d2ce311
Erik Michaels-Ober <sferik@gmail.com>
2011-12-15 00:59:29 -0800

    Add method for list timeline

--- lib/t/cli/list.rb
+++ lib/t/cli/list.rb
@@ -1,3 +1,4 @@
+require 'action_view'
 require 't/rcfile'
 require 'thor'
 require 'twitter'
@@ -5,6 +6,8 @@ require 'twitter'
 module T
   class CLI
     class List < Thor
+      include ActionView::Helpers::DateHelper
+
       DEFAULT_HOST = 'api.twitter.com'
       DEFAULT_PROTOCOL = 'https'
 
@@ -23,6 +26,21 @@ module T
         list = client.list_create(listname, hash)
         say "@#{@rcfile.default_profile[0]} created the list: #{list.name}."
       end
+
+      desc "timeline LISTNAME", "Show tweet timeline for members of the specified list."
+      method_option :number, :aliases => "-n", :type => :numeric, :default => 20
+      method_option :reverse, :aliases => "-r", :type => :boolean, :default => false
+      def timeline(listname)
+        hash = {}
+        hash.merge!(:per_page => options['number']) if options['number']
+        timeline = client.list_timeline(listname, hash)
+        timeline.reverse! if options['reverse']
+        run_pager
+        timeline.map do |status|
+          say "#{status.user.screen_name.rjust(20)}: #{status.text} (#{time_ago_in_words(status.created_at)} ago)"
+        end
+      end
+      map %w(tl) => :timeline
 
       desc "add SUBCOMMAND ...ARGS", "Add users to a list."
       require 't/cli/list/add'
@@ -56,6 +74,33 @@ module T
 
       def protocol
         parent_options['no_ssl'] ? 'http' : DEFAULT_PROTOCOL
+      end
+
+      def run_pager
+        return if RUBY_PLATFORM =~ /win32/
+        return if ENV["T_ENV"] == "test"
+        return unless STDOUT.tty?
+
+        read, write = IO.pipe
+
+        unless Kernel.fork # Child process
+          STDOUT.reopen(write)
+          STDERR.reopen(write) if STDERR.tty?
+          read.close
+          write.close
+          return
+        end
+
+        # Parent process, become pager
+        STDIN.reopen(read)
+        read.close
+        write.close
+
+        ENV['LESS'] = 'FSRX' # Don't page if the input is short enough
+
+        Kernel.select [STDIN] # Wait until we have input before we start the pager
+        pager = ENV['PAGER'] || 'less'
+        exec pager rescue exec "/bin/sh", "-c", pager
       end
 
     end

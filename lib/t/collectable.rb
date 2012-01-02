a7f6bb61e260ebd8beb94b76e65e3708fc7a3f23
Erik Michaels-Ober <sferik@gmail.com>
2012-01-02 00:11:19 -0800

    Tighten up Collectable#collect_with_cursor

--- lib/t/collectable.rb
+++ lib/t/collectable.rb
@@ -2,10 +2,9 @@ module T
   module Collectable
 
     def collect_with_cursor(collection=[], cursor=-1, &block)
-      return collection if cursor == 0
       object = yield cursor
       collection += object.collection
-      collect_with_cursor(collection, object.next_cursor)
+      object.next_cursor.zero? ? collection : collect_with_cursor(collection, object.next_cursor)
     end
 
   end

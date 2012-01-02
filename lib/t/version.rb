34730881c76143496eb7c052eb1a1f6dfed6b540
Erik Michaels-Ober <sferik@gmail.com>
2012-01-01 23:55:05 -0800

    Bump version to 0.3.0

--- lib/t/version.rb
+++ lib/t/version.rb
@@ -8,12 +8,12 @@ module T
 
     # @return [Integer]
     def self.minor
-      2
+      3
     end
 
     # @return [Integer]
     def self.patch
-      1
+      0
     end
 
     # @return [String, NilClass]

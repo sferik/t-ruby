926b3c9b7b3af9d6ea6d4d42b49b8bc0990a6459
Steve Agalloco <steve.agalloco@gmail.com>
2012-08-25 00:40:22 -0400

    tests from Stream#matrix and Stream#all

--- spec/stream_spec.rb
+++ spec/stream_spec.rb
@@ -1,6 +1,10 @@
 require 'helper'
 
 describe T::Stream do
+  before :all do
+    @status = status_from_fixture("status.json")
+  end
+
   before :each do
     T::RCFile.instance.path = fixture_path + "/.trc"
 
@@ -15,17 +19,61 @@ describe T::Stream do
     context '--csv' do
       before :each do
         @stream.options = @stream.options.merge("csv" => true)
-        @tweetstream_client.stub(:on_timeline_status).
-          and_yield(fixture("status.json"))
+        @stream.stub(:say).and_return
       end
 
-      it "should output in CSV format" do
+      it 'outputs headings when the stream initializes' do
+        @tweetstream_client.stub(:on_timeline_status).and_return
+        @tweetstream_client.stub(:on_inited).and_yield
+
+        @stream.should_receive(:say).with(any_args)
+        @stream.all
+      end
+
+      it "outputs in CSV format" do
+        @tweetstream_client.stub(:on_inited).and_return
+        @tweetstream_client.stub(:on_timeline_status).
+          and_yield(@status)
+
         @stream.should_receive(:print_csv_status).with(any_args)
         @stream.all
       end
     end
 
     context '--long' do
+      before :each do
+        @stream.options = @stream.options.merge("long" => true)
+      end
+
+      it 'outputs headings when the stream initializes' do
+        @tweetstream_client.stub(:on_inited).and_yield
+        @tweetstream_client.stub(:on_timeline_status).and_return
+        STDOUT.stub(:tty?).and_return(true)
+
+        @stream.should_receive(:print_table).with(any_args)
+        @stream.all
+      end
+
+      it "outputs in long text format" do
+        @tweetstream_client.stub(:on_inited).and_return
+        @tweetstream_client.stub(:on_timeline_status).
+          and_yield(@status)
+
+        @stream.should_receive(:print_table).with(any_args)
+        @stream.all
+      end
+    end
+
+    context 'normal usage' do
+      before :each do
+        @tweetstream_client.stub(:on_timeline_status).
+          and_yield(@status)
+      end
+
+      it 'prints the tweet status' do
+        @stream.should_receive(:print_message)
+        @stream.all
+      end
     end
 
     it 'invokes TweetStream::Client.sample' do
@@ -33,4 +81,22 @@ describe T::Stream do
       @stream.all
     end
 	end
+
+  describe '#matrix' do
+    before :each do
+      @tweetstream_client.stub(:on_timeline_status).
+        and_yield(@status)
+    end
+
+    it 'outputs the tweet status' do
+      @stream.should_receive(:say).with(any_args)
+      @stream.matrix
+    end
+
+    it 'invokes TweetStream::Client.sample' do
+      @tweetstream_client.should_receive(:sample)
+      @stream.stub(:say).and_return
+      @stream.matrix
+    end
+  end
 end

7735f732198b3bd8123e6cd85c42cdf2d70960f7
Erik Michaels-Ober <sferik@gmail.com>
2012-11-20 15:54:31 -0800

    Rename local variable

--- lib/t/collectable.rb
+++ lib/t/collectable.rb
@@ -23,15 +23,15 @@ module T
       tweets.empty? ? collection.flatten : collect_with_max_id(collection, tweets.last.id - 1, &block)
     end
 
-    def collect_with_count(number, &block)
+    def collect_with_count(count, &block)
       opts = {}
       opts[:count] = MAX_NUM_RESULTS
       collect_with_max_id do |max_id|
         opts[:max_id] = max_id unless max_id.nil?
-        opts[:count] = number unless number >= MAX_NUM_RESULTS
-        if number > 0
+        opts[:count] = count unless count >= MAX_NUM_RESULTS
+        if count > 0
           tweets = yield opts
-          number -= tweets.length
+          count -= tweets.length
           tweets
         end
       end.flatten.compact

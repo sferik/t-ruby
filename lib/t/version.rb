2945810ae9532743169cfea0b95bbca21002e32d
ngatnicdotmil <yeganyan@hotmail.com>
2013-12-22 16:32:10 +0400

    Update version.rb
    Probably not the most elegant solution, but it works. shows the most recent tagged releases in repository.

--- lib/t/version.rb
+++ lib/t/version.rb
@@ -1,30 +1,38 @@
-module T
-  class Version
-    class << self
-      # @return [Integer]
-      def major
-        2
-      end
+class Version
+  include Comparable
 
-      # @return [Integer]
-      def minor
-        1
-      end
+  attr_reader :major, :feature_group, :feature, :bugfix
 
-      # @return [Integer]
-      def patch
-        0
-      end
+  def initialize(version="")
+    v = version.split(".")
+    @major = v[0].to_i
+    @feature_group = v[1].to_i
+    @feature = v[2].to_i
+    @bugfix = v[3].to_i
+  end
+  
+  def <=>(other)
+    return @major <=> other.major if ((@major <=> other.major) != 0)
+    return @feature_group <=> other.feature_group if ((@feature_group <=> other.feature_group) != 0)
+    return @feature <=> other.feature if ((@feature <=> other.feature) != 0)
+    return @bugfix <=> other.bugfix
+  end
 
-      # @return [String, NilClass]
-      def pre
-        nil
-      end
+  def self.sort
+    self.sort!{|a,b| a <=> b}
+  end
 
-      # @return [String]
-      def to_s
-        [major, minor, patch, pre].compact.join('.')
-      end
-    end
+  def to_s
+    @major.to_s + "." + @feature_group.to_s + "." + @feature.to_s + "." + @bugfix.to_s
   end
 end
+
+## Example Usage:
+##  list = []
+##  ["1.2.2.10","1.2.2.1","2.1.10.2","2.3.4.1"].each {|v| list.push(Version.new(v)) }
+##  list.sort.each{|v| pp v.to_s }
+##
+###"1.2.2.1"
+###"1.2.2.10"
+###"2.1.10.2"
+###"2.3.4.1"

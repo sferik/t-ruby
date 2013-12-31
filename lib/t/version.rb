97a5dc2155fa67f2a784bfbd97f72568fd3a21a1
Erik Michaels-Ober <sferik@gmail.com>
2013-12-31 20:41:47 +0100

    Replace methods with constants in T::Version class

--- lib/t/version.rb
+++ lib/t/version.rb
@@ -1,29 +1,14 @@
 module T
   class Version
+    MAJOR = 2
+    MINOR = 2
+    PATCH = 0
+    PRE = nil
+
     class << self
-      # @return [Integer]
-      def major
-        2
-      end
-
-      # @return [Integer]
-      def minor
-        2
-      end
-
-      # @return [Integer]
-      def patch
-        0
-      end
-
-      # @return [String, NilClass]
-      def pre
-        nil
-      end
-
       # @return [String]
       def to_s
-        [major, minor, patch, pre].compact.join('.')
+        [MAJOR, MINOR, PATCH, PRE].compact.join('.')
       end
     end
   end

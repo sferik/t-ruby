7a440705e2781860705fb0b6d12ee974f9b52888
Erik Michaels-Ober <sferik@gmail.com>
2014-01-08 17:55:00 +0100

    Remove premature optimization

--- lib/t/utils.rb
+++ lib/t/utils.rb
@@ -111,12 +111,7 @@ module T
     end
 
     def parallel_map(enumerable)
-      # Don't bother spawning a new thread if there's only one item
-      if enumerable.count == 1
-        enumerable.map { |object| yield object }
-      else
-        enumerable.map { |object| Thread.new { yield object } }.map(&:value)
-      end
+      enumerable.map { |object| Thread.new { yield object } }.map(&:value)
     end
   end
 end

b40e2088971811a83f917b22bc928ed8ee30e21b
Erik Berlin <sferik@gmail.com>
2017-11-18 07:56:35 -0800

    Changed scope of BASE_URL

--- lib/t/cli.rb
+++ lib/t/cli.rb
@@ -79,7 +79,7 @@ module T
       open_or_print('https://apps.twitter.com', dry_run: options['display-uri'])
       key = ask 'Enter your API key:'
       secret = ask 'Enter your API secret:'
-      consumer = OAuth::Consumer.new(key, secret, site: Twitter::REST::Client::BASE_URL)
+      consumer = OAuth::Consumer.new(key, secret, site: Twitter::REST::Request::BASE_URL)
       request_token = consumer.get_request_token
       uri = generate_authorize_uri(consumer, request_token)
       say
@@ -973,7 +973,7 @@ module T
         value =~ /"(.*?)"/
         "#{key}=#{CGI.escape(Regexp.last_match[1])}"
       end.join('&')
-      "#{Twitter::REST::Client::BASE_URL}#{request.path}?#{params}"
+      "#{Twitter::REST::Request::BASE_URL}#{request.path}?#{params}"
     end
 
     def pin_auth_parameters
